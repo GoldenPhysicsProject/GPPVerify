9f0c26a969dc4b498a930742551316858d65150a
Daniel Toupin <dtoupin@goldenphysics.org>
2026-09-22 09:02:35 -0400

    Fix Matrix.Notation import for Mathlib 4.33

--- GppVerify/StandardModel/UnitarityForcesOrientationAlignment.lean
+++ GppVerify/StandardModel/UnitarityForcesOrientationAlignment.lean
@@ -1,5 +1,5 @@
 import Mathlib.Tactic
-import Mathlib.Data.Matrix.Notation
+import Mathlib.LinearAlgebra.Matrix.Notation
 
 /-!
 # Positivity/unitarity forces alignment of symplectic and frequency orientations
